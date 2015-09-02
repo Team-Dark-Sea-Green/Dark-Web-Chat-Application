7d3d60667cb5c41c2e3e2cec23724046614e4f7c
Extensa <ragnar245@gmail.com>
2015-09-02 16:12:31 +0200

    Seed method update

--- DarkWebChat/DarkWebChat.Data/Migrations/Configuration.cs
+++ DarkWebChat/DarkWebChat.Data/Migrations/Configuration.cs
@@ -1,3 +1,8 @@
+using System.Collections.Generic;
+using DarkWebChat.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
 namespace DarkWebChat.Data.Migrations
 {
     using System;
@@ -15,6 +20,60 @@ namespace DarkWebChat.Data.Migrations
 
         protected override void Seed(WebChatContext context)
         {
+            if (!context.Users.Any())
+            {
+                UserStore<ApplicationUser> userStore = new UserStore<ApplicationUser>(context);
+                UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>(userStore);
+
+                var userA = new ApplicationUser()
+                {
+                    Email = "richard@mail.com",
+                    UserName = "Batman",
+                    PasswordHash = new PasswordHasher().HashPassword("Password1!")
+                };
+                var userB = new ApplicationUser()
+                {
+                    Email = "william@mail.co.uk",
+                    UserName = "Robin",
+                    PasswordHash = new PasswordHasher().HashPassword("Password1!")
+                };
+
+                context.Users.Add(userA);
+                context.Users.Add(userB);
+                context.SaveChanges();
+
+                userManager.UpdateSecurityStamp(userA.Id);
+                userManager.UpdateSecurityStamp(userB.Id);
+                context.SaveChanges();
+
+                var messages = new List<ChannelMessage>()
+                {
+                    new ChannelMessage()
+                    {
+                        Content = "Wassup Bats!?",
+                        Date = DateTime.Now,
+                        Sender = context.Users.FirstOrDefault(u => u.UserName == "Robin"),
+                        IsFile = false
+                    },
+                    new ChannelMessage()
+                    {
+                        Content = "I'm Batman!",
+                        Date = DateTime.Now,
+                        Sender = context.Users.FirstOrDefault(u => u.UserName == "Batman"),
+                        IsFile = false
+                    }
+                };
+
+                var channel = new Channel()
+                {
+                    Name = "Channel-1",
+                    Users = context.Users.Where(u => u.UserName == "Batman" || u.UserName == "Robin").ToList(),
+                    Messages = messages
+                };
+
+                context.Channels.Add(channel);
+                context.SaveChanges();
+            }
         }
     }
 }
